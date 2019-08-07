1d460bac7365263b19f72a6b39f5e90f4871ecbd
Team JAAS <enggarjunnaik@gmail.com>
2019-08-07 15:09:34 -0400

    Delete showhide-password.directive.spec.ts

--- showhide-password.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowhidePasswordDirective } from './showhide-password.directive';
-
-describe('ShowhidePasswordDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowhidePasswordDirective();
-    expect(directive).toBeTruthy();
-  });
-});
